d2ab3dd54863983763111ac3003cd9d6d867e61c
AlbertGlez97 <albert.gonzalez0297@gmail.com>
2025-09-17 11:33:15 -0600

    feat: Anadiendo el metodo create a nuestra clase ProductsService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');// Prefijo global para todas las rutas
+
+  // Configuración global del ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // Lanza un error si hay propiedades no definidas
+      transform: true, // Transforma los payloads a los tipos definidos en los DTOs
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}/api`); // Muestra la URL completa con el prefijo
 }
 bootstrap();
